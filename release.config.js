c562be462ccad3f9af8ba17738fbf7dbb62dd51b
Holf <holf@holf.net>
2023-10-21 22:24:40 +0100

    fix: restore npm-publish plugin

--- release.config.js
+++ release.config.js
@@ -2,8 +2,11 @@ module.exports = {
     // This will be changed to 'main' once we're happy with everything
     branches: "prep-for-publish",
     plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
+        "@semantic-release/git",
         [
-            "@semantic-release/git",
             {
                 assets: ["package.json"], // Add other files you want to commit here
                 message:

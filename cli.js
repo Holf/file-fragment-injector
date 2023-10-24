a4e969a22942146fea630c846011c57bd98fdb89
Holf <holf@holf.net>
2023-10-24 21:06:35 +0100

    feat: typescript typing

--- cli.js
+++ cli.js
@@ -2,11 +2,11 @@
 
 const { injectFileFragments } = require("./src/index.js");
 
-const [sourceFilePath, destFilePath] = process.argv.slice(2);
+const [templateFilePath, generatedFilePath] = process.argv.slice(2);
 
-if (!sourceFilePath || !destFilePath) {
+if (!templateFilePath || !generatedFilePath) {
     console.error("You must provide a Source File Path and a Destination File Path.");
     process.exit(1);
 }
 
-injectFileFragments(sourceFilePath, destFilePath);
+injectFileFragments(templateFilePath, generatedFilePath);
---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+declare module "@drtrt/inject-file-fragments" {
+    function injectFileFragments(templateFilePath: string, generatedFilePath: string): void;
+
+    export { injectFileFragments };
+}

aadc504a00e35854bd17173f532dba05eac5f82e
Holf <holf@holf.net>
2023-10-25 14:13:37 +0100

    feat: changelog generation
    This is a test, which we will run on the dev branch, to see
    what release notes look like.

--- release.config.js
+++ release.config.js
@@ -1,14 +1,20 @@
 module.exports = {
     // This will be changed to 'main' once we're happy with everything
-    branches: "main",
+    branches: "generate-release-notes",
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/npm",
-        "@semantic-release/git",
         [
+            "@semantic-release/changelog",
             {
-                assets: ["package.json"], // Add other files you want to commit here
+                changelogFile: "CHANGELOG.md",
+            },
+        ],
+        "@semantic-release/npm",
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json", "CHANGELOG.md"], // Add other files you want to commit here
                 message:
                     "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
             },

a94b29295e6bf35cd3440298bedf3fc064b927a7
Holf <holf@holf.net>
2023-10-25 15:02:36 +0100

    ci: comments for PR and Issues during release

--- release.config.js
+++ release.config.js
@@ -18,6 +18,22 @@ module.exports = {
                     "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
             },
         ],
+        [
+            "@semantic-release/github",
+            {
+                successComment:
+                    "This PR has been included in release version ${{nextRelease.version}} :tada:",
+                failComment: "The release failed with the following error: ${{error.message}}",
+                releasedLabels: ["released"], // Label to add to released issues and PRs
+                issue: {
+                    successComment:
+                        "This issue has been resolved in version ${{nextRelease.version}} :tada:",
+                    failComment:
+                        "There was a problem with the release of version ${{nextRelease.version}}, that should have addressed this issue. The error was: ${{error.message}}",
+                    failTitle: "Release failed for version ${{nextRelease.version}}",
+                },
+            },
+        ],
     ],
     releaseRules: [
         {
